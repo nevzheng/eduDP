3672dada176a9c36e1c55bcb0eecfc4ac57e0ac1
Nevin Zheng <nevzheng@gmail.com>
2021-07-17 21:11:18 -0400

    Completed E

--- src/E_knapsack2.cpp
+++ src/E_knapsack2.cpp
@@ -4,10 +4,25 @@
 
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <vector>
 using namespace std;
 
-long knapsack2(const vector<long>& val, const vector<long>& wgt, long N, long W) { return 0; }
+int64_t knapsack2(const vector<long> &val, const vector<long> &wgt, long N, long W) {
+    auto maxVal = accumulate(begin(val), end(val), 0L);
+    vector<long> dp(maxVal + 1, 1e18L + 5);// dp[i] the minimum total weight of items with total value exactly i
+    dp[0] = 0;
+    for (auto item = 0; item < N; item++) {
+        for (auto value_already = maxVal - val[item]; value_already >= 0; value_already--) {
+            dp[value_already + val[item]] = min(dp[value_already + val[item]], dp[value_already] + wgt[item]);
+        }
+    }
+    auto ans = 0L;
+    for (auto i = 0l; i <= maxVal; i++) {
+        if (dp[i] <= W) ans = max(ans, i);
+    }
+    return ans;
+}
 
 // TODO: Complete knapsack2
 int main() {
